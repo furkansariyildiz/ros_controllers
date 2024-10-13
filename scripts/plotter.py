b660e24c60e4e6d3200250b2c175bcf937c08713
furkansariyildiz <furkansariyildiz@windowslive.com>
2024-10-13 21:47:35 +0300

    feat: plotter is added for each controllers

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -8,26 +8,43 @@ import csv
 
 class Plotter:
     def __init__(self):
-        self.time = []
-        self.position = []
-        self.velocity = []
-        self.effort = []
+        self.x_result_ = []
+        self.y_result_ = []
+        self.x_desired_ = []
+        self.y_desired_ = []
 
 
     
-    def read_csv(self, file_path):
-        with open(file_path, 'r') as csvfile:
+    def read_csv(self, result_path_filename, desired_path_filename):
+        with open(result_path_filename, 'r') as csvfile:
             plots = csv.reader(csvfile, delimiter=',')
+            next(plots)
             for row in plots:
-                self.time.append(float(row[0]))
-                self.position.append(float(row[1]))
-                self.velocity.append(float(row[2]))
-                self.effort.append(float(row[3]))
+                print("row[0]: ", row[0])
+                print("row[1]: ", row[1])
+                self.x_result_.append(float(row[0]))  
+                self.y_result_.append(float(row[1]))
 
-        plt.plot(self.time, self.position, label='position')
-        plt.xlabel('time')
-        plt.ylabel('position')
-        plt.title('Position vs Time')
+        with open(desired_path_filename, 'r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            next(plots)
+            for row in plots:
+                print("row[0]: ", row[0])
+                print("row[1]: ", row[1])
+                self.x_desired_.append(float(row[0]))  
+                self.y_desired_.append(float(row[1]))
+
+        # Plot result x and y positions
+        plt.plot(self.x_result_, self.y_result_, label='Result Path', color='blue')
+
+        # Plot desired x and y positions
+        plt.plot(self.x_desired_, self.y_desired_, label='Desired Path', color='red')
+
+        plt.xlabel('X position')
+        plt.ylabel('Y position')
+        plt.title('Desired and Result Position')
+        plt.legend()
+        plt.grid(True)
         plt.show()
 
 
@@ -35,5 +52,5 @@ class Plotter:
 
 if __name__ == '__main__':
     plotter = Plotter()
-    plotter.read_csv('joint_states.csv')
+    plotter.read_csv('/home/furkan/controller_ws/src/ros_controllers/results/result_pure_pursuit.csv', '/home/furkan/controller_ws/src/ros_controllers/results/desired_pure_pursuit.csv')
 
